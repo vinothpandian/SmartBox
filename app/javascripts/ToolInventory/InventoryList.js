adb9ec7f29e18f2e0ea256ad30a00dcdd2cb23b1
Vinoth <vinothpandian@live.com>
2017-06-29 21:29:35 +0200

    Updated inventory manager

--- app/javascripts/ToolInventory/InventoryList.js
+++ app/javascripts/ToolInventory/InventoryList.js
@@ -18,67 +18,6 @@ class InventoryList extends React.Component {
       <div className="card-block">
         <h4 className="card-title">There are <span className="text-muted" id="noOfTools"></span> tools in your inventory</h4>
       </div>
-      <table className="table table-hover mx-auto w-90">
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Tool Name</th>
-            <th>Tool ID</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>Wire (5m)</td>
-            <td className>0xac60e905167f51a083f252f36abcdef123456789</td>
-            <td className>
-              <a href="#" className="btn btn-outline-danger m-0 btn-sm p-1">
-                X
-              </a>
-            </td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Wire (5m)</td>
-            <td>0xac60e905167f51a083f252f36abcdef123456789</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td>Wire (5m)</td>
-            <td>0xac60e905167f51a083f252f36abcdef123456789</td>
-          </tr>
-          <tr>
-            <td>4</td>
-            <td>Wire (5m)</td>
-            <td>0xac60e905167f51a083f252f36abcdef123456789</td>
-          </tr>
-          <tr>
-            <td>5</td>
-            <td>Wire (5m)</td>
-            <td>0xac60e905167f51a083f252f36abcdef123456789</td>
-          </tr>
-          <tr>
-            <td>6</td>
-            <td>Wire (5m)</td>
-            <td>0xac60e905167f51a083f252f36abcdef123456789</td>
-          </tr>
-          <tr>
-            <td>7</td>
-            <td>Wire (5m)</td>
-            <td>0xac60e905167f51a083f252f36abcdef123456789</td>
-          </tr>
-          <tr>
-            <td>8</td>
-            <td>Wire (5m)</td>
-            <td>0xac60e905167f51a083f252f36abcdef123456789</td>
-          </tr>
-          <tr>
-            <td>9</td>
-            <td>Wire (5m)</td>
-            <td>0xac60e905167f51a083f252f36abcdef123456789</td>
-          </tr>
-        </tbody>
-      </table>
     </div>
   )
   }
